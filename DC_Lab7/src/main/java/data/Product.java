4546a3eb20dd50fd9ec01d4078104bec993ba8c1
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-11-16 19:35:04 +0200

    added class Product

---
+++ DC_Lab7/src/main/java/data/Product.java
@@ -0,0 +1,25 @@
+package data;
+
+public class Product {
+    public int code;
+    public String name;
+    public Section section;
+    public Product(int code, String name, Section section){
+        this.code = code;
+        this.name = name;
+        this.section = section;
+    }
+
+    public String getProductName() {
+        return this.name;
+    }
+
+    public Integer getProductCode(){
+        return this.code;
+    }
+
+    public Integer getSectionProductCode(){
+        return this.section.getSectionCode();
+    }
+
+}
