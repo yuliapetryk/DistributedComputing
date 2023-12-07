2e14ab25bba8353cd920041725c40632711b0626
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-12-08 01:29:36 +0200

    update the connection between the server and the client

--- DC_Lab8/src/main/java/Socket/Client.java
+++ DC_Lab8/src/main/java/Socket/Client.java
@@ -1,17 +1,102 @@
 package Socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    private  final Socket sock;
+    private static PrintWriter out;
+    private static  BufferedReader in;
+
+    public Client(String ip, int port) throws IOException {
+        sock = new Socket(ip, port);
+        in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+        out = new PrintWriter(sock.getOutputStream(), true);
+    }
 
     public static void main(String[] args) {
-        Socket sock = null;
         try {
-            System.out.println("Connecting to server...");
-            sock = new Socket("localhost", 12345);
-            System.out.println("Connected");
+            Client client = new Client("localhost", 12345);
+            showSections();
+            readResponse();
+            getProductByName("DRESS");
+            readResponse();
+            stop();
+            client.disconnect();
         } catch (IOException e) {
+            System.out.println("Error");
             e.printStackTrace();
         }
     }
+
+    private static String sendRequest(String... requests) {
+        StringBuilder request = new StringBuilder();
+        for (String arg : requests) {
+            request.append(arg).append("#");
+        }
+       return request.toString();
+    }
+
+    private static void readResponse() throws IOException {
+        String response = in.readLine();
+        String[] fields = response.split("#");
+        for (String field : fields) {
+            System.out.println(field);
+        }
+        System.out.println();
+    }
+
+    public static void addSection(String id, String name) throws IOException {
+        out.println(sendRequest("1", id, name));
+    }
+
+    public static void deleteSection(String id) throws IOException {
+        out.println(sendRequest("2", id));
+    }
+
+    public static void updateSection( String id, String name) throws IOException {
+        out.println(sendRequest("3", id, name));
+    }
+
+    public static void addProduct( String id, String name, String price, String section) throws IOException {
+        out.println(sendRequest("4", id, name, section, price));
+    }
+
+    public static void deleteProduct(String id) throws IOException {
+        out.println(sendRequest("5", id));
+    }
+
+    public static void updateProduct(String id, String name, String price, String section) throws IOException {
+        out.println(sendRequest("6", id, name, section, price));
+    }
+
+    public static void showProductCountInSection(String id) throws IOException {
+        out.println(sendRequest("7", id));
+    }
+
+    public static void getProductByName(String name) throws IOException {
+        out.println(sendRequest("8",name));
+    }
+
+    public static void showProductInSection(String id) throws IOException {
+        out.println(sendRequest("9", id));
+    }
+
+    public static void showSections() throws IOException {
+        out.println(sendRequest("10"));
+    }
+
+    public static void stop() throws IOException {
+        out.println(sendRequest("11"));
+    }
+
+
+    public void disconnect() throws IOException {
+        sock.close();
+    }
+
 }
--- DC_Lab8/src/main/java/Socket/Server.java
+++ DC_Lab8/src/main/java/Socket/Server.java
@@ -1,25 +1,73 @@
 package Socket;
 
+import Database.DatabaseService;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-public class Server {
-    public static void main(String[] args) {
-        ServerSocket server = null;
-        try {
-            server = new ServerSocket(12345);
-        } catch (
-                IOException e) {
-            e.printStackTrace();
-        }
-        Socket sock = null;
-        try {
-            System.out.println("Waiting for a client ...");
-            sock = server.accept();
-            System.out.println("Client connected");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+import java.sql.SQLException;
 
+public class Server {
+    private static ServerSocket server = null;
+    private static Socket sock = null;
+    private static PrintWriter out = null;
+    private static BufferedReader in = null;
+
+    public static void main(String[] args) {
+        try {
+            Server srv = new Server();
+            srv.start(12345);
+        } catch (IOException | SQLException | ClassNotFoundException | InstantiationException |
+                 IllegalAccessException e) {
+            System.out.println("Error");
+        }
+    }
+
+    public void start(int port) throws IOException, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        server = new ServerSocket(port);
+        while (true) {
+            sock = server.accept();
+            in = new BufferedReader(new
+                    InputStreamReader(sock.getInputStream()));
+            out = new PrintWriter(sock.getOutputStream(), true);
+            while (processQuery()) ;
+        }
+    }
+
+    public static boolean processQuery() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        DatabaseService service = new DatabaseService("jdbc:mysql://localhost:3306/shop", "root", "06102003");
+        try {
+            String query = in.readLine();
+            String[] parameters = query.split("#");
+            String command = parameters[0];
+            String response = "";
+            switch (command) {
+                case "1" -> response = service.addSection(Integer.parseInt(parameters[1]), parameters[2]);
+                case "2" -> response = service.deleteSection(Integer.parseInt(parameters[1]));
+                case "3" -> response = service.updateSection(Integer.parseInt(parameters[1]), parameters[2]);
+                case "4" -> response = service.addProduct(Integer.parseInt(parameters[1]), parameters[2], Integer.parseInt(parameters[3]), Integer.parseInt(parameters[4]));
+                case "5" -> response = service.deleteProduct(Integer.parseInt(parameters[1]));
+                case "6" -> response = service.updateProduct(Integer.parseInt(parameters[1]), parameters[2], Integer.parseInt(parameters[3]), Integer.parseInt(parameters[4]));
+                case "7" -> response = service.showProductCountInSection(Integer.parseInt(parameters[1]));
+                case "8" -> response = service.getProductByName(parameters[1]);
+                case "9" -> response = service.showProductInSection(Integer.parseInt(parameters[1]));
+                case "10" -> response = service.showSections();
+                case "11" -> {
+                    sock.close();
+                    in.close();
+                    out.close();
+                    return false;
+                }
+            }
+            out.println(response);
+            out.flush();
+            return true;
+        } catch (Exception e) {
+            System.out.println("SERVER: Error: " + e.getMessage());
+            return false;
+        }
     }
 }
