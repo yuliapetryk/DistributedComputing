88c704dcf78a3eb6cce135b8d9d93964b7d4d1c0
Yulia Petryk <yulya.petruk.03@gmail.com>
2023-12-08 21:48:00 +0200

    created Server

--- DC_Lab8/src/main/java/RMI/Server.java
+++ DC_Lab8/src/main/java/RMI/Server.java
@@ -1,18 +1,16 @@
 package RMI;
 
-import javax.management.remote.rmi.RMIServerImpl;
+import java.rmi.AlreadyBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.sql.SQLException;
 
 public class Server {
-    public static void main(String[] args) {
-        try {
-            Registry r = LocateRegistry.createRegistry(1099);
-           // RMIServerImpl server = new RMIServerImpl();
-            //r.rebind("SayHello", server);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws RemoteException, InterruptedException, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, AlreadyBoundException {
+        final ShopServerImpl server = new ShopServerImpl();
+        Registry registry = LocateRegistry.createRegistry(123);
+        registry.rebind("Shop", server);
+       Thread.sleep(Integer.MAX_VALUE);
     }
 }
